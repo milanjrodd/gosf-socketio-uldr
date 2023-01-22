9e511a2a05ef6b45b734c085bf391ad003541945
Milan Rodd <milanrodd@mail.ru>
2023-01-22 21:31:15 +0400

    FIX msg types: uldr compatible

--- handler.go
+++ handler.go
@@ -2,7 +2,6 @@ package gosocketio
 
 import (
 	"encoding/json"
-	"reflect"
 	"sync"
 
 	"github.com/ambelovsky/gosf-socketio/protocol"
@@ -14,12 +13,14 @@ const (
 	OnError         = "error"
 )
 
-/**
+/*
+*
 System handler function for internal event processing
 */
 type systemHandler func(c *Channel)
 
-/**
+/*
+*
 Contains maps of message processing functions
 */
 type methods struct {
@@ -30,14 +31,16 @@ type methods struct {
 	onDisconnection systemHandler
 }
 
-/**
+/*
+*
 create messageHandlers map
 */
 func (m *methods) initMethods() {
 	//m.messageHandlers = make(sync.Map)
 }
 
-/**
+/*
+*
 Add message processing function, and bind it to given method
 */
 func (m *methods) On(method string, f interface{}) error {
@@ -50,7 +53,8 @@ func (m *methods) On(method string, f interface{}) error {
 	return nil
 }
 
-/**
+/*
+*
 Find message processing function associated with given method
 */
 func (m *methods) findMethod(method string) (*caller, bool) {
@@ -77,63 +81,29 @@ func (m *methods) callLoopEvent(c *Channel, event string) {
 	f.callFunc(c, &struct{}{})
 }
 
-/**
+/*
+*
 Check incoming message
 On ack_resp - look for waiter
 On ack_req - look for processing function and send ack_resp
 On emit - look for processing function
 */
 func (m *methods) processIncomingMessage(c *Channel, msg *protocol.Message) {
-	switch msg.Type {
-	case protocol.MessageTypeEmit:
-		f, ok := m.findMethod(msg.Method)
-		if !ok {
-			return
-		}
-
-		if !f.ArgsPresent {
-			f.callFunc(c, &struct{}{})
-			return
-		}
-
-		data := f.getArgs()
-		err := json.Unmarshal([]byte(msg.Args), &data)
-		if err != nil {
-			return
-		}
-
-		f.callFunc(c, data)
-
-	case protocol.MessageTypeAckRequest:
-		f, ok := m.findMethod(msg.Method)
-		if !ok || !f.Out {
-			return
-		}
-
-		var result []reflect.Value
-		if f.ArgsPresent {
-			//data type should be defined for unmarshall
-			data := f.getArgs()
-			err := json.Unmarshal([]byte(msg.Args), &data)
-			if err != nil {
-				return
-			}
-
-			result = f.callFunc(c, data)
-		} else {
-			result = f.callFunc(c, &struct{}{})
-		}
-
-		ack := &protocol.Message{
-			Type:  protocol.MessageTypeAckResponse,
-			AckId: msg.AckId,
-		}
-		send(ack, c, result[0].Interface())
-
-	case protocol.MessageTypeAckResponse:
-		waiter, err := c.ack.getWaiter(msg.AckId)
-		if err == nil {
-			waiter <- msg.Args
-		}
+	f, ok := m.findMethod(msg.Method)
+	if !ok {
+		return
 	}
+
+	if !f.ArgsPresent {
+		f.callFunc(c, &struct{}{})
+		return
+	}
+
+	data := f.getArgs()
+	err := json.Unmarshal([]byte(msg.Args), &data)
+	if err != nil {
+		return
+	}
+
+	f.callFunc(c, data)
 }
